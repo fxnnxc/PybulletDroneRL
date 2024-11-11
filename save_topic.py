4d83d424ac5b9481bc90d6b199d4984bf7a4ffe5
fxnnxc <bumjin@kaist.ac.kr>
2024-11-11 13:29:14 +0900

    update the code

--- save_topic.py
+++ save_topic.py
@@ -1,47 +1,73 @@
 #!/usr/bin/env python
 import rospy
 import json
-from std_msgs.msg import String  # 예시로 String 메시지 타입 사용
+from std_msgs.msg import String
 from datetime import datetime
+import time
+from collections import deque
 
 class MultiTopicToJson:
     def __init__(self):
         rospy.init_node('multi_topic_to_json_node', anonymous=True)
         
-        # JSON 파일 경로 설정
+        # 샘플링 설정
+        self.sampling_rate = 5  # 1초당 저장할 샘플 수
+        self.sampling_interval = 1.0 / self.sampling_rate  # 샘플링 간격 (초)
+        self.recording_duration = 6000  # 총 녹화 시간 (초)
+        
+        # 임시 버퍼 설정
+        self.temp_buffers = {}
+        
         self.json_file = f"ros_data_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
         self.data_dict = {}
+        self.last_save_time = {}
         
         # 구독할 토픽 리스트
         self.topic_list = [
             '/rostopic/aaa',
             '/rostopic/bbb',
-            # 필요한 만큼 토픽을 추가하세요
         ]
         
-        # 각 토픽별 데이터 저장소 초기화
+        # 초기화
         for topic in self.topic_list:
             self.data_dict[topic] = []
+            self.temp_buffers[topic] = deque(maxlen=1)  # 최신 데이터만 유지
+            self.last_save_time[topic] = time.time()
             
-        # 각 토픽에 대한 구독자 설정
+        self.start_time = time.time()
+        
+        # 구독자 설정
         self.subscribers = [
             rospy.Subscriber(topic, String, self.callback, callback_args=topic)
             for topic in self.topic_list
         ]
         
+        # 타이머 설정 (샘플링 간격으로 저장)
+        rospy.Timer(rospy.Duration(self.sampling_interval), self.save_sample)
+        
     def callback(self, msg, topic_name):
-        # 받은 메시지를 딕셔너리 형태로 변환
-        data = {
+        # 최신 데이터를 임시 버퍼에 저장
+        self.temp_buffers[topic_name].append({
             'timestamp': rospy.get_time(),
-            'data': msg.data  # 실제 메시지 구조에 맞게 수정하세요
-        }
+            'data': msg.data
+        })
         
-        # 해당 토픽의 데이터 리스트에 추가채
-        self.data_dict[topic_name].append(data)
+    def save_sample(self, event):
+        current_time = time.time()
         
-        # JSON 파일로 저장
-        self.save_to_json()
-        
+        # 녹화 시간 체크
+        if current_time - self.start_time > self.recording_duration:
+            self.save_to_json()
+            rospy.signal_shutdown("Recording completed")
+            return
+            
+        # 각 토픽에 대해 샘플링 간격마다 데이터 저장
+        for topic in self.topic_list:
+            if self.temp_buffers[topic]:  # 버퍼에 데이터가 있는 경우
+                # 가장 최신 데이터 가져오기
+                latest_data = self.temp_buffers[topic][-1]
+                self.data_dict[topic].append(latest_data)
+                
     def save_to_json(self):
         with open(self.json_file, 'w') as f:
             json.dump(self.data_dict, f, indent=4)
